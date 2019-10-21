e1ee30b94fb5bc582a49f130628a57fedfbf9bdc
khyiao0829 <iao0829@naver.com>
2019-10-21 10:36:56 +0900

    test1

---
+++ 201920996-midterm-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (void)
+{
+  double in1, in2;
+  double average
+
+  scanf("%d %d", &in1, &in2);
+
+  average = (in1+in2)/2;
+
+  printf("average = %d", average);
+
+  return 0;
+
+ }
